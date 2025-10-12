ca966251871df65f8fb46b1b791be769d99d0e52
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-10-12 10:29:16 +0200

    fix: add linkPlugin to MarkdownEditor component plugins

--- src/renderer/src/components/markdown-editor.tsx
+++ src/renderer/src/components/markdown-editor.tsx
@@ -1,11 +1,11 @@
 import {
   headingsPlugin,
+  linkPlugin,
   listsPlugin,
   markdownShortcutPlugin,
   MDXEditor,
   quotePlugin
 } from '@mdxeditor/editor'
-
 import '@mdxeditor/editor/style.css'
 import { cn } from '@renderer/utils'
 
@@ -14,7 +14,13 @@ export const MarkdownEditor = () => {
     <div>
       <MDXEditor
         markdown={'# Hello World'}
-        plugins={[headingsPlugin(), listsPlugin(), quotePlugin(), markdownShortcutPlugin()]}
+        plugins={[
+          headingsPlugin(),
+          linkPlugin(),
+          listsPlugin(),
+          quotePlugin(),
+          markdownShortcutPlugin()
+        ]}
         contentEditableClassName={cn(
           'caret-yellow-500 prose dark:prose-invert min-h-screen text-lg px-8 py-5 outline-none max-w-nonet',
           "prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"

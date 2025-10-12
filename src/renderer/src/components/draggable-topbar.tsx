172102e34ee11e35de6cdf1a60577588e644ce70
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-10-12 08:46:05 +0200

    feat: implement DraggableTopbar component with transparent header

--- src/renderer/src/components/draggable-topbar.tsx
+++ src/renderer/src/components/draggable-topbar.tsx
@@ -1,5 +1,5 @@
 type Props = {}
 
 export const DraggableTopbar = (props: Props) => {
-  return <header className="absolute inset-0 h-8 border bg-transparent" />
+  return <header className="absolute inset-0 h-8 bg-transparent" />
 }

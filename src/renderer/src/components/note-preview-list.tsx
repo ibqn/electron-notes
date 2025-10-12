89a3445ced13251c4344af62e0370ee16009f66a
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-10-12 09:22:14 +0200

    fix: handle empty notes state in NotePreviewList component; display message when no notes are available

--- src/renderer/src/components/note-preview-list.tsx
+++ src/renderer/src/components/note-preview-list.tsx
@@ -5,6 +5,14 @@ import { NotePreview } from './note-preview'
 type Props = ComponentProps<'ul'>
 
 export const NotePreviewList = (props: Props) => {
+  if (mockNotes.length === 0) {
+    return (
+      <ul {...props} className="p-4 text-center">
+        <span>No notes yet</span>
+      </ul>
+    )
+  }
+
   return (
     <ul {...props}>
       {mockNotes.map((note) => (

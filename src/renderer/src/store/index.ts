5afb9c4e8fbf5d48c6b8651274a11fec54f30e96
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-10-12 21:59:00 +0200

    feat: implement notes state management with jotai atoms

---
+++ src/renderer/src/store/index.ts
@@ -0,0 +1,18 @@
+import { NoteInfo } from '@shared/models'
+import { atom } from 'jotai'
+import { mockNotes } from './mocks'
+
+export const notesAtom = atom<NoteInfo[]>(mockNotes)
+
+export const selectedNoteIndexAtom = atom<number | null>(null)
+
+export const selectedNoteAtom = atom((get) => {
+  const notes = get(notesAtom)
+  const selectedNoteIndex = get(selectedNoteIndexAtom)
+  if (selectedNoteIndex === null) {
+    return null
+  }
+  const selectedNote = notes[selectedNoteIndex]
+
+  return { ...selectedNote, content: `# ${selectedNote.title}\n\nStart writing your note here...` }
+})

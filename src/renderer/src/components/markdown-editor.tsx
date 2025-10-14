19547977c74104949f5342befd6985c269e1442b
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-10-14 18:07:15 +0200

    fix: update contentEditableClassName to use min-h-full for better layout

--- src/renderer/src/components/markdown-editor.tsx
+++ src/renderer/src/components/markdown-editor.tsx
@@ -34,7 +34,7 @@ export const MarkdownEditor = () => {
           markdownShortcutPlugin()
         ]}
         contentEditableClassName={cn(
-          'caret-yellow-500 prose dark:prose-invert min-h-screen text-lg px-8 py-5 outline-none max-w-nonet',
+          'caret-yellow-500 prose dark:prose-invert min-h-full text-lg px-8 py-5 outline-none max-w-nonet',
           "prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
         )}
       />

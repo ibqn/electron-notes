5aa5d37a77f84cb2dc97e8d8726e6b003f47aa42
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-10-16 11:01:58 +0200

    fix: update getRootDir and file path handling to use path.join for better cross-platform compatibility

--- src/main/lib/index.ts
+++ src/main/lib/index.ts
@@ -4,10 +4,11 @@ import { ensureDir, readdir, readFile, stat } from 'fs-extra'
 import { homedir } from 'os'
 import { randomUUID } from 'crypto'
 import type { GetNotes, ReadNote } from '@shared/types'
+import path from 'path'
 
 export const uuid = (): string => randomUUID()
 
-export const getRootDir = () => `${homedir()}/${appDirectoryName}`
+export const getRootDir = () => path.join(homedir(), appDirectoryName)
 
 export const getNotes: GetNotes = async () => {
   const notesDir = getRootDir()
@@ -25,7 +26,7 @@ export const getNotes: GetNotes = async () => {
 }
 
 export const getNoteInfoFromFileName = async (fileName: string): Promise<NoteInfo> => {
-  const fileStats = await stat(`${getRootDir()}/${fileName}`)
+  const fileStats = await stat(path.join(getRootDir(), fileName))
 
   return {
     id: uuid(),
@@ -37,7 +38,7 @@ export const getNoteInfoFromFileName = async (fileName: string): Promise<NoteInf
 export const readNote: ReadNote = async (fileName: string): Promise<string> => {
   const rootDir = getRootDir()
 
-  return readFile(`${rootDir}/${fileName}.md`, {
+  return readFile(path.join(rootDir, `${fileName}.md`), {
     encoding: fileEncoding
   })
 }
